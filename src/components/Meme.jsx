70b1a8e9842e95a763d68d2d9ff04c03b4acfa11
Ankur Kumar <akit9815@gmail.com>
2023-06-21 18:11:52 +0530

    fetching random meme image and updating UI using react state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,8 +1,23 @@
 import "./Meme.css"
+import memesData from "../memesData"
+import { useState } from "react"
 export default function Meme () {
+    
+    
+    const [memeImage,setMemeImage] = useState("")
+    
+
+    function onClickHandler() {
+
+        
+        const randomMemeIndex = Math.floor(Math.random()*(memesData.data.memes.length + 1) )
+        const newImageUrl = memesData.data.memes[randomMemeIndex].url
+        setMemeImage(newImageUrl)
+
+    }
     return (
         <main className= 'main'>
-            <form className="form">
+            <div className="form">
                 <input 
                     className = "form--input"
                     type="text"
@@ -13,8 +28,9 @@ export default function Meme () {
                     type="text"
                     placeholder="Phrase 2">
                 </input>
-                <button className="form--button">Get me a new meme !</button>
-            </form>
+                <button className="form--button" onClick={onClickHandler}>Get me a new meme !</button>
+                <img src={memeImage} className="meme--image"/>
+            </div>
         </main>
     )
 }
