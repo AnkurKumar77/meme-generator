3ca0c5ff22e779f27b0a165390f1ac098280b41c
Ankur Kumar <akit9815@gmail.com>
2023-06-22 16:18:56 +0530

    added text overlay and updates using states

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,32 +4,62 @@ import { useState } from "react"
 export default function Meme () {
     
     
-    const [memeImage,setMemeImage] = useState("")
+    const [memeImage,setMemeImage] = useState({
+        topText:"",
+        bottomText:"",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
     
+    function handleChange(event)
+    {
+        const {name,type,value} = event.target;
+        setMemeImage(prevMemeImageData=>({
+            ...prevMemeImageData,
+            [name]:value
 
-    function onClickHandler() {
-
+        }))
+    }
+    function submitHandler(event) {
+        console.log("called")
+        event.preventDefault();
+        const randomMemeIndex = Math.floor(Math.random()*(memesData.data.memes.length + 1) );
+        const newImageUrl = memesData.data.memes[randomMemeIndex].url;
         
-        const randomMemeIndex = Math.floor(Math.random()*(memesData.data.memes.length + 1) )
-        const newImageUrl = memesData.data.memes[randomMemeIndex].url
-        setMemeImage(newImageUrl)
+        setMemeImage(prevMemeImageData=>({
+            ...prevMemeImageData,
+            imageUrl:newImageUrl
+
+        }))
 
     }
     return (
         <main className= 'main'>
-            <div className="form">
+            <form className="form" onSubmit={submitHandler}>
                 <input 
                     className = "form--input"
                     type="text"
-                    placeholder="Phrase 1">   
+                    placeholder="Phrase 1"
+                    name="topText"
+                    value={memeImage.topText}
+                    onChange={handleChange}
+                    >   
                 </input>
                 <input    
                     className = "form--input"
                     type="text"
-                    placeholder="Phrase 2">
+                    placeholder="Phrase 2"
+                    name="bottomText"
+                    value={memeImage.bottomText}
+                    onChange={handleChange}
+                    >
                 </input>
-                <button className="form--button" onClick={onClickHandler}>Get me a new meme !</button>
-                <img src={memeImage} className="meme--image"/>
+                <button className="form--button" >Get me a new meme !</button>
+
+            </form>
+            <div className="meme-zone">
+                <img src={memeImage.imageUrl} className="meme--image"/>
+                <h2 className="meme--text top">{memeImage.topText}</h2>
+                <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
             </div>
         </main>
     )
